09324c32660d16943e86f4c7ef4fd2d4d2ee1aed
amiDMast <amidmast@gmail.com>
2019-12-26 17:53:09 +0200

    upload

---
+++ aws-docker-web.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+sudo echo "Start install $(date)" >> /log.txt
+sudo yum install docker -y
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo docker-compose --version
+
+sudo cat > /etc/docker/daemon.json <<EOF
+{
+  "storage-driver": "devicemapper"
+}
+EOF
+
+
+sudo service docker start
+sudo chkconfig docker on
+sudo echo "$(docker info)" >> /log.txt
+sudo yum install git -y
+
+sudo mkdir /DOCKER/
+sudo cd /DOCKER/
+sudo git clone http://gitlab.lenex.com.ua/root/docker-nginx.git
+sudo mv ./docker-nginx/* .
+sudo rm -rf ./docker-nginx
+sudo docker-compose build
+sudo docker-compose up -d
+sudo echo "Finish install $(date)" >> /log.txt
