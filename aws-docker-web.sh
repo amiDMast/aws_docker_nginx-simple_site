53e7fc2126a0517d48c02d4b9dcaf62106bb824a
Dmitry Chernov <amidmast@gmail.com>
2020-04-26 22:39:00 +0300

    Update aws-docker-web.sh

--- aws-docker-web.sh
+++ aws-docker-web.sh
@@ -1,28 +1,17 @@
 #!/bin/bash
-sudo echo "Start install $(date)" >> /log.txt
-sudo yum install docker -y
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-sudo docker-compose --version
-
-sudo cat > /etc/docker/daemon.json <<EOF
-{
-  "storage-driver": "devicemapper"
-}
-EOF
-
-
-sudo service docker start
-sudo chkconfig docker on
-sudo echo "$(docker info)" >> /log.txt
-sudo yum install git -y
-
-sudo mkdir /DOCKER/
-sudo cd /DOCKER/
-sudo git clone https://github.com/amiDMast/docker-nginx.git
-sudo mv ./docker-nginx/* .
-sudo rm -rf ./docker-nginx
-sudo docker-compose build
-sudo docker-compose up -d
-sudo echo "Finish install $(date)" >> /log.txt
+echo "Start install $(date)" >> /log.txt
+yum update -y
+yum install docker git -y
+curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+service docker start
+chkconfig docker on
+mkdir /DOCKER/
+cd /DOCKER/
+git clone https://github.com/amiDMast/docker-nginx.git
+mv ./docker-nginx/* /DOCKER/
+rm -rf ./docker-nginx
+docker-compose build
+docker-compose up -d
+echo "Finish install $(date)" >> /log.txt
